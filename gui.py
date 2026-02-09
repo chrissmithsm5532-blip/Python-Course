5f3bcbb628d6332a1ff2887a242059773b761e04
marbl <chris.smith.sm5532@gmail.com>
2026-02-09 13:52:33 +0000

    Write to file and close GUI

--- gui.py
+++ gui.py
@@ -1,11 +1,26 @@
 import functions
-import FreeSimpleGUI as sg
+import FreeSimpleGUI as Sg
 
-label =sg.Text("Type in a To-Do")
-input_box = sg.InputText(tooltip = "Enter todo")
-add_button =  sg.Button("Add")
+label =Sg.Text("Type in a To-Do")
+input_box = Sg.InputText(tooltip = "Enter todo",key="todo")
+add_button =  Sg.Button("Add")
 
-window = sg.Window("My To-Do App",layout= [[label],[input_box,add_button]])
-window.read()
-print("hello")
+window = Sg.Window("My To-Do App",
+                   layout= [[label],[input_box,add_button]],
+                   font=("Helvetica",20))
+
+while True:
+    event,values = window.read()
+    print(event,values)
+    print(values["todo"])
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            print(todos)
+        case Sg.WIN_CLOSED:
+            break
+
 window.close()
