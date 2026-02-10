539814e460dea27918840a358a579759e95cf5e7
marbl <chris.smith.sm5532@gmail.com>
2026-02-10 15:15:28 +0000

    Basic GUI Complete and Exit Buttons

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 import functions
 import FreeSimpleGUI as Sg
 
+from functions import write_todos
+
 label =Sg.Text("Type in a To-Do")
 input_box = Sg.InputText(tooltip = "Enter todo",key="todo")
 add_button =  Sg.Button("Add")
@@ -8,10 +10,12 @@ list_box = Sg.Listbox(functions.get_todos(),key="todos",
                       enable_events=True,
                       size=(45,10))
 edit_button = Sg.Button("Edit")
+complete_button = Sg.Button("Complete")
+exit_button = Sg.Button("Exit",key="Exit")
 
 
 window = Sg.Window("My To-Do App",
-                   layout= [[label],[input_box,add_button],[list_box,edit_button]],
+                   layout= [[label],[input_box,add_button],[list_box,edit_button,complete_button],[exit_button]],
                    font=("Helvetica",20))
 
 while True:
@@ -33,6 +37,16 @@ while True:
             window["todos"].update(values=todos)
         case "todos":
             window["todo"].update(value=values["todos"][0])
+        case "Complete":
+            todo_complete = values['todos'][0]
+            print(todo_complete)
+            todos = functions.get_todos()
+            todos.remove(todo_complete)
+            functions,write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(values="")
+        case "Exit":
+            break
         case Sg.WIN_CLOSED:
             break
 
